989a23500f518318deda80938338584035a5c15b
brianchan <cxb2000abc@gmail.com>
2022-01-10 14:34:06 +0800

    commit

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template, request
+import logging #python building in package
 import pandas as pd
+from sqlalchemy import create_engine
+import redis
+
+#r = redis.Redis('18.216.168.246', port=6379, password='user')
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(name)s|%(message)s')
+my_logger = logging.getLogger('brian')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def query():
+    #my_logger.warning('write log to bigquery')
+    print(my_logger.name)
     if request.method == 'POST':
         ac_type = request.form.get('Air-Con Type')
         brand = request.form.get('Brand Name')
         budget = request.form.get('Budget')
         hp = request.form.get('Horse Power')
 
-        if len(ac_type) == 0 and len(brand) == 0 and len(budget) == 0 and len(hp) == 0:
+        if len(ac_type) + len(brand) + len(budget) + len(hp) == 0:
             return render_template('request_form.html', message='Please at least one criteria.', ac_type=ac_type,
                                    brand=brand, budget=budget, hp=hp)
 
-        if len(budget)>0:
+        if len(budget) > 0:
             try:
                 budget = float(budget)
             except:
@@ -29,26 +40,26 @@ def query():
                 return render_template('request_form.html', message='Detect invalid input.', ac_type=ac_type,
                                    brand=brand, budget=budget, hp=hp)
 
-        df = pd.read_csv('air_con_db.csv')
-        df['price'] = df['price'].astype('float')
-        df['hp'] = df['hp'].astype('float')
+        sql_engine = create_engine(f'mysql+pymysql://user:user@18.216.168.246/ac', pool_recycle=3600)
+        db_connection = sql_engine.connect()
+        df = pd.read_sql("select * from ac.ac_view", db_connection)
 
-        if df[df['ac_type'] == ac_type].shape[0] == 0:
+        if ac_type != '' and df[df['ac_type'] == ac_type].shape[0] == 0:
             return render_template('request_form.html', message='No that type', ac_type=ac_type,
                                    brand=brand, budget=budget, hp=hp)
 
-        if df[df['brand'] == brand].shape[0] == 0:
+        if brand != '' and df[df['brand'] == brand].shape[0] == 0:
             return render_template('request_form.html', message='No that brand', ac_type=ac_type,
                                    brand=brand, budget=budget, hp=hp)
 
-        if len(request.form.get('Air-Con Type'))>0:
+        if len(request.form.get('Air-Con Type')) > 0:
             df = df[df['ac_type'] == ac_type]
 
-        if len(request.form.get('Brand Name'))>0:
+        if len(request.form.get('Brand Name')) > 0:
             df = df[df['brand'] == brand]
 
         if len(request.form.get('Budget')) > 0:
-            df = df[df['price']<float(request.form.get('Budget'))]
+            df = df[df['price'] < float(request.form.get('Budget'))]
 
         if len(request.form.get('Horse Power')) > 0:
             df = df[df['hp'] == float(request.form.get('Horse Power'))]
@@ -57,9 +68,9 @@ def query():
             return render_template('request_form.html', message='Sorry no match air-con', ac_type=ac_type,
                                    brand=brand, budget=budget, hp=hp)
 
-        message = {'result': df.reset_index(drop=True).iloc[0:3].T.to_dict()}
+        tables = df.reset_index(drop=True).iloc[0:3].to_dict('records')
 
-        return render_template('request_form.html',message=message, ac_type=ac_type,
-                                   brand=brand, budget=budget, hp=hp)
+        return render_template('request_form.html', message='', ac_type=ac_type,
+                                   brand=brand, budget=budget, hp=hp, tables=tables)
 
-    return render_template('request_form.html',message='')
+    return render_template('request_form.html', message='')
--- main.py
+++ main.py
@@ -2,144 +2,171 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
+import numpy as np
+import time
+from sqlalchemy import create_engine
+import pymysql
 
-url = 'https://www.broadwaylifestyle.com/hk_en/air-conditioner?p=product_list_order=general_categories'
-source = requests.get('https://www.broadwaylifestyle.com/hk_en/air-conditioner?product_list_order=general_categories')
-
-soup = BeautifulSoup(source.text,'html.parser')
-
-total_nos = int(soup.select_one('p.toolbar-amount').text.strip().split('of')[-1].strip())
-
-grid = soup.select_one("ol.products.list.items.product-items")
-air_con = grid.select('li')
-len(air_con)
-
-#--------------test--------------------#
-
-air_con_link = []
-for i in air_con:
-    link = i.select_one('a')['href']
-    air_con_link.append(link)
-
-print(air_con_link)
-
-#--------------test--------------------#
-
-page = 1
-air_con_links = []
-while True:
-        print(f'now on page {page}')
-        url = f'https://www.broadwaylifestyle.com/hk_en/air-conditioner?p={page}&product_list_order=general_categories'
-        source = requests.get(url)
-        soup = BeautifulSoup(source.text, 'html.parser')
-        grid = soup.select_one("ol.products.list.items.product-items")
-        air_con = grid.select('li')
-        for i in air_con:
-            link = i.select_one('a')['href']
-            air_con_links.append(link)
-            if len(air_con_links) >= 100:
-                break
-        if len(air_con_links) >= 100:
-            break
-        page +=1
-
-#--------------test--------------------#
-
-print(air_con_links)
-url1 = air_con_links[0]
-source1 = requests.get(url1)
-soup1 = BeautifulSoup(source1.text, 'html.parser')
-
-status = soup1.select_one('.product-info-stock-sku').text.strip()
-
-name = soup1.select_one('.page-title').text.strip()
-print(name)
-
-find_hp = re.compile('([0-9.]+)\s?(?=HP)',re.I)
-hp = find_hp.search(name).group(1)
-print(hp)
-
-find_type = re.compile('([a-zA-Z]+)[ -]?(?=type)',re.I)
-ac_type = find_type.search('MS-22CRFB 2.5HP Inverter Split-type Air-conditioner').group(1)
-print(ac_type)
-
-
-price = soup1.select_one('.normal-price span.price').text
-
-#cover_area = soup.select_one('tbody [data-th="Suggest Coverage area"]').text.split('-')
-#print(cover_area)
-
-brand = soup1.select_one('tbody [data-th="Brand"]').text
-
-main_feature = []
-feature1 = soup1.select_one('tbody [data-th="Brand"]').text
-main_feature.append(feature1)
-feature_no =2
-while True:
-    try:
-        feature = soup1.select_one(f'tbody [data-th="Main Feature {feature_no}"]').text
-        main_feature.append(feature)
-        feature_no += 1
-    except:
-        break
-
-#--------------test--------------------#
-
-air_con_dataset = []
-ac_no = 1
-for link in air_con_links:
-    print(link)
-    url = link
-    source = requests.get(url)
-    soup = BeautifulSoup(source.text, 'html.parser')
-
-    status = soup.select_one('.product-info-stock-sku').text.strip()
-
-    name = soup.select_one('.page-title').text.strip()
-    print(name)
-
-    try:
-        find_hp = re.compile('([0-9./]+)\s?(?=HP)',re.I)
-        hp = find_hp.search(name).group(1)
-    except:
-        hp = None
-
-    try:
-        find_type = re.compile('([a-zA-Z]+)[ -]?(?=Type)', re.I)
-        ac_type = find_type.search(name).group(1)
-        print(ac_type)
-    except:
-        ac_type = None
-
-    price = soup.select_one('.normal-price span.price').text.split('$')[1].replace(',','')
-
-    #cover_area = soup.select_one('tbody [data-th="Suggest Coverage area"]').text.split('-')
-    #max_cover_area = cover_area[1]
-    #min_cover_area = cover_area[0]
-
-    brand = soup.select_one('tbody [data-th="Brand"]').text.strip()
-
-    main_feature = []
-    feature1 = soup.select_one('tbody [data-th="Main Feature"]').text
-    main_feature.append(feature1)
-    feature_no = 2
-    while True:
+def request_with_retry(url, headers):
+    try_nos = 1
+    while try_nos < 3:
         try:
-            feature = soup.select_one(f'tbody [data-th="Main Feature {feature_no}"]').text
-            main_feature.append(feature)
-            feature_no += 1
+            source0 = requests.get(url, headers=headers, timeout=20)
+            return source0
         except:
-            break
-    air_con_dataset.append([name,status,hp,ac_type,price,brand,main_feature,url])
-    print(f'AC no.{ac_no}')
-    ac_no += 1
+            print('retry')
+            time.sleep(20 * try_nos)
+            try_nos += 1
+    raise ValueError('connection fail')
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
+
+url = 'https://www.broadwaylifestyle.com/hk_en/air-conditioner?p=1&product_list_dir=desc'
+source = request_with_retry(url, headers)
+soup = BeautifulSoup(source.text, 'html.parser')
+total_nos = int(soup.select_one('p.toolbar-amount').text.strip().split(' of ')[1])
 
 
-print(air_con_dataset)
+# get all individual air-conditioner url
+url_list = []
+page_no = 1
+while len(url_list) < total_nos:
+    print(f'now on page{page_no}')
 
-df = pd.DataFrame(air_con_dataset, columns=['name','status','hp','ac_type','price','brand','main_feature','link'])
-df['hp'].replace('3/4','0.75',inplace=True)
-df.to_csv('air_con_db.csv',index=False)
+    page_url = f'https://www.broadwaylifestyle.com/hk_en/air-conditioner?p={page_no}&product_list_dir=desc'
+    page_source = request_with_retry(page_url, headers)
+    page_soup = BeautifulSoup(page_source.text, 'html.parser')
 
+    blocks = page_soup.select('li.item.product.product-item')
+    for block in blocks:
+        url_list.append(block.select_one('div.product-item-info a')['href'])
+
+    page_no += 1
+
+
+# get individual air-conditioner information
+area_pattern = re.compile('Coverage Area', re.I)
+type_pattern = re.compile('(\w+)[\s-]?type', re.I)
+hp_pattern = re.compile('([\d./]+)HP\s', re.I)
+
+total_info_list = []
+
+counter = 0
+for ac_url in url_list[counter:]:
+    print(f'aircon number: {counter}')
+
+    ac_source = request_with_retry(ac_url, headers)
+    ac_soup = BeautifulSoup(ac_source.text, 'html.parser')
+
+    info_table = ac_soup.select_one('table', id='product-attribute-specs-table')
+
+    try:
+        product_name = info_table.find(attrs={"data-th": "Product Name"}).text.strip()
+
+        if re.search(type_pattern, product_name): # exclude potable or unknown type aircon
+            price = ac_soup.select_one('div.price').text.strip().split('$')[1].replace(',', '')
+
+            ac_type = re.search(type_pattern, product_name).group(1).lower()
+
+            horsepower = re.search(hp_pattern, product_name).group(1)
+
+            broadway_code = info_table.find(attrs={"data-th": "Broadway Code"}).text.strip()
+
+            brand_name = info_table.find(attrs={"data-th": "Brand"}).text.strip()
+
+            model_no = info_table.find(attrs={"data-th": "Model"}).text.strip()
+
+            feature1 = info_table.find(attrs={"data-th": "Main Feature"}).text.strip()
+
+            try:
+                feature2 = info_table.find(attrs={"data-th": "Main Feature 2"}).text.strip()
+            except:
+                feature2 = None
+
+            try:
+                feature3 = info_table.find(attrs={"data-th": "Main Feature 3"}).text.strip()
+            except:
+                feature3 = None
+
+            suggest_area = info_table.find(attrs={"data-th": area_pattern}).text.strip()
+            print(suggest_area)
+            if 'or below' in suggest_area:
+                suggest_area_low_limit, suggest_area_up_limit = 0, suggest_area.split()[0]
+            else:
+                suggest_area_low_limit, suggest_area_up_limit = suggest_area.split('-')
+
+            total_info_list.append([ac_url, model_no, brand_name, ac_type, horsepower, price, broadway_code
+                                    , suggest_area_low_limit, suggest_area_up_limit, feature1, feature2, feature3])
+    except:
+        print('page not found')
+
+    counter += 1
+
+len(url_list)
+
+info_df = pd.DataFrame(total_info_list, columns=['url', 'model_no', 'brand_name', 'ac_type', 'horsepower', 'price'
+                                                 , 'broadway_code', 'suggest_area_low_limit', 'suggest_area_up_limit'
+                                                 , 'feature1', 'feature2', 'feature3'])
+
+info_df['horsepower'] = info_df['horsepower'].apply(lambda x: x.replace('3/4', '0.75'))
+info_df['horsepower'] = info_df['horsepower'].astype('float')
+info_df[['price', 'suggest_area_low_limit', 'suggest_area_up_limit']] = \
+    info_df[['price', 'suggest_area_low_limit', 'suggest_area_up_limit']].astype('int')
+
+info_df.to_csv('new_info.csv', index=False)
+info_df = pd.read_csv('new_info.csv')
+info_df.rename(columns={'suggest_area_low_limit': 'area_low_lim', 'suggest_area_up_limit': 'area_up_lim'}, inplace=True)
+test = pd.DataFrame({'url':['a', 'b'], 'model_no': ['aaa', 'bbb'], 'brand_name': ['aaa', 'bbb'], 'ac_type': ['aaa', 'bbb']
+                    , 'horsepower': [1, 2], 'price': [300, 100], 'broadway_code': ['aaa', 'bbb']
+                    , 'suggest_area_low_limit': [10, 30], 'suggest_area_up_limit': [20, 40]
+                    , 'feature1': ['aaa', 'bbb'], 'feature2': ['aaa', 'bbb'], 'feature3': ['aaa', 'bbb']})
+
+new_df = pd.read_csv('new_info.csv')
+new_df
+info_df1 = info_df.iloc[:21]
+info_df2 = info_df.iloc[21:51]
+info_df3 = info_df.iloc[51:101]
+info_df4 = info_df.iloc[101:131]
+info_df5 = info_df.iloc[131:141]
+info_df6 = info_df.iloc[141:145]
+info_df7 = info_df.iloc[145:146]
+info_df.iloc[146]
+info_df8 = info_df.iloc[146:148]
+sql_engine = create_engine(f'mysql+pymysql://user:user@18.216.168.246/ac', pool_recycle=3600)
+db_connection = sql_engine.connect()
+info_df8.to_sql('info', db_connection, if_exists='append', index=False)
+db_connection.close()
+
+total_status_list = []
+
+counter = 0
+for ac_url in url_list[counter:]:
+    print(f'aircon number: {counter}')
+
+    ac_source = request_with_retry(ac_url, headers)
+    ac_soup = BeautifulSoup(ac_source.text, 'html.parser')
+
+    info_table = ac_soup.select_one('table', id='product-attribute-specs-table')
+
+    try:
+        product_name = info_table.find(attrs={"data-th": "Product Name"}).text.strip()
+
+        if re.search(type_pattern, product_name): # exclude potable or unknown type aircon
+            status = ac_soup.find(attrs={"title": "Availability"}).text.strip()
+            print(status)
+
+            total_status_list.append([ac_url, status])
+    except:
+        print('page not found')
+
+    counter += 1
+
+status_df = pd.DataFrame(total_status_list, columns=['url', 'stock_status'])
+status_df.to_csv('new_status.csv', index=False)
+
+sql_engine = create_engine(f'mysql+pymysql://user:user@18.216.168.246/ac', pool_recycle=3600)
+db_connection = sql_engine.connect()
+status_df.to_sql('stock', db_connection, if_exists='append', index=False)
+db_connection.close()
 
 
